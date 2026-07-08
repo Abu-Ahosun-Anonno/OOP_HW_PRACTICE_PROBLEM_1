3a8ab612f428c2c3fb97bf7d2ff65ec3ed3e6bd1
Abu-Ahosun-Anonno <anonnodj2@gmail.com>
2026-07-09 00:32:28 +0600

    Add files via upload
    problem g & problem h

---
+++ practice_problem_1_g.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class practice_problem_1_g {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a number =");
+        int Number=input.nextInt();
+        if(Number>=0){
+            System.out.println("Positive");
+        }
+        else{
+            System.out.println("Not Positive");
+        }
+
+
+
+
+    }
+}
---
+++ practice_problem_1_h.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class practice_problem_1_h {
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter a number =");
+        int Number=input.nextInt();
+        if(Number%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+
+
+
+
+    }
+}
